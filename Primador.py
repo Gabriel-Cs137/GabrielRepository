2d55409cdb9b3abb47c242675ac9633c75b10427
Gabriel-Cs137 <gabrielpaganotti176@gmail.com>
2024-10-15 10:50:02 -0400

    Código reduzido e otmizado, prompt melhorado

--- Primador.py
+++ Primador.py
@@ -1,79 +1,57 @@
+nm = 0
+primo = []
+primo2 = [2,3,5,7]
+primer = False
+def pedir_numero():
+    global nm
+    while True:
+        try:
+            nm = int(input('Insira um número: '))
+            break
+        except:
+            print('Valor inválido, tente novamente.')
+    print('=====================================================')
 
-def primos(nm):
-            global primo
-            
-            if nm == 2:
-                print('É Primo')
-                primo.append(nm)
+def calc(type,nm):
+    global primo, primer, primo2
+    
+    if type == 1:
+        if nm <= 1:
+            primer = False
+        elif nm == 2 or nm == 3 or nm == 5 or nm == 7:
+            primer = True
+        else:
 
-            
-            elif nm <= 1:
-                print('Não é primo')
-                
-
-            elif nm == 3:
-                print('É primo')
-                primo.append(nm)
-
-
-            elif nm == 5:
-                print('É primo')
-                primo.append(nm)
-
-
-            elif nm == 7:
-                print('É primo')
-                primo.append(nm)
-
-
-            elif nm == 11:
-                print('É primo')
-                primo.append(nm)
-
-
-            else: 
-                if nm % 2 != 0 and nm % 3 != 0 and nm % 5 != 0 and nm % 7 != 0 and nm % 11 != 0 :
-                    primador = True
-                    if primos != []:
-                        for x in primo:
-                            if nm % x != 0:
-                                pass
-                            elif nm % x == 0:
-                                primador = False
-                                print('Não é primo.')
-                                break
-                    if primador == True:
-                        print('É primo.')
-                        primo.append(nm)
-                        print(f'primos encontrados: {primo}')
-                    else:
-                        print('Não é primo')
+            for x in primo2:
+                if nm % x != 0:
+                    primer = True
                 else:
-                    print('Não é primo')
-
-while True:
-    verif = input('Deseja testar um range de números ou um número especifico? \n[N = Fechar|A = Número especifico|R = Range]: ')
-    if verif == 'N':
-        break
-    elif verif == 'A':
-        while True:
-            try:
-                nm =int(input('Insira o número que deseja verficar: '))
-                break
-            except:
-                print('Insira um valor válido')
-            
-        primos(nm)
+                    primer = False
+                    break
+            if primer == True:
+                primo2.append(nm)
+                primo.append(nm)
 
     else:
-        while True:
-            try:
-                nm =int(input('Insira a quantidade de número que deseja verficar: '))
-                break
-            except:
-                print('Insira um valor válido')
-
-        primo = []
-        
-        for y in range(nm):
-            primos(y)
+        pass
+while True:
+        verif = input('O que gostaria de fazer?\n[1- Verificar se um número é primo]\n[2-Verificar quais numeros dentro de um range]\n[3- Fechar o sistema]\n[]:')
+        if verif == '1':
+            pedir_numero()
+            calc(1,nm)
+            if primer == True:
+                print('é um número primo')
+            else:
+                print('Não é um número primo')
+            print('=====================================================')
+        elif verif == '2':
+            pedir_numero()
+            primo = []
+            for x in range(nm):
+                calc(1,x)
+            print(f'primos encontrados: {primo}')
+            print('=====================================================')
+        elif verif == '3':
+            break
+        else:
+            print('Opção inválida, tente novamente.')
